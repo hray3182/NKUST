f6c7f75a15cf3657a359d84b18845cd5c03d2314
hray <hray3182@gmail.com>
2024-05-06 23:32:32 +0800

    finish homework

---
+++ 1st-2/php/week11/homework/wrong_table/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pwd = "";
+$db_name = "student";
+$db_table = "studentHAHAHA"; // 故意寫錯
+$dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
+$conn = new PDO($dsn, $db_user, $db_pwd);
+
+$query_sql = "SELECT * FROM $db_table";
+
+$stmt = $conn->prepare($query_sql);
+$result = $stmt->execute();
+if ($result) {
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $response['status'] = 200; //OK;
+    $response['message'] = "查詢成功";
+    $response['result'] = $rows;
+} else {
+    $response['status'] = 500; //Server Error;
+    $response['message'] = "查詢失敗";
+}
+echo json_encode($response);
