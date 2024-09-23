01eaac962869e24e816414ba6a27ae49f6106bd8
HRay <hray3182@gmail.com>
2024-09-24 00:38:53 +0800

    添加 java week2 作業

---
+++ 2st-1/java/homework_1_1.java
@@ -0,0 +1,6 @@
+public class homework_1_1 {
+    public static void main(String[] args) {
+        System.out.println("蘇泓叡");
+        System.out.println("C112156233");
+    }
+}
---
+++ 2st-1/java/homework_1_2.java
@@ -0,0 +1,9 @@
+public class homework_1_2 {
+    public static void main(String[] args) {
+        String id = "C112156233";
+        String name = "蘇泓叡";
+        int age = 28;
+        char sex = '男';
+        System.out.printf("%s 同學你好\n你的學號是%s\n你的年齡是%d歲\n你目前的性別是%c性\n", name, id, age, sex);
+    }
+}
